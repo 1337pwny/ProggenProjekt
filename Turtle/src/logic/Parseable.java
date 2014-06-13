e0f092bc0f4b282021e6d237fb0627345c02b807
ITGrufti <info@core-control.de>
2014-06-13 12:21:37 +0200

    Fixed forward and backwards; removed int return from parseable

--- Turtle/src/logic/Parseable.java
+++ Turtle/src/logic/Parseable.java
@@ -3,5 +3,5 @@ package logic;
 
 public interface Parseable {
 	public String getName();
-	public int moveTurtle(Turtle workTurtle, String[] args);
+	public void moveTurtle(Turtle workTurtle, String[] args);
 }
--- Turtle/src/parsers/Forward.java
+++ Turtle/src/parsers/Forward.java
@@ -10,9 +10,20 @@ public class Forward implements Parseable {
 	}
 
 	@Override
-	public int moveTurtle(Turtle workTurtle, String[] args) {
+	public void moveTurtle(Turtle workTurtle, String[] args) {
 		Position workPosition=workTurtle.getActualPosition();
-		
+		int distance=0;
+		//Testing for a real number
+		try{
+			distance=Integer.parseInt(args[1]);
+		}//Else its assumed to be a variable --> Fail: Throw unknowVariableException
+		catch(NumberFormatException e){
+			distance=workTurtle.getVariable(args[1]);
+		}
+		//setting the positions
+		workPosition.setPositionX((int)(workPosition.getPositionX()+distance*Math.cos(Math.PI/180 * workPosition.getAngle())));
+		workPosition.setPositionY((int)(workPosition.getPositionY()+distance*Math.sin(Math.PI/180 * workPosition.getAngle())));
+		workTurtle.setPosition(workPosition);
 	}
 	
 }
